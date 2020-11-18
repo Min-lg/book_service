c49a0dcc05ff66c0673c10055322749d1f171287
gaoliming111 <616436112@qq.com>
2020-11-18 19:47:24 +0800

    代码改正

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 全路由的配置，解决跨域问题，需在路由路径配置之前执行：
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-length, Authorization, Accept, X-Requested-With, yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if(req.method == 'OPTIONS')
+    res.send(200);
+  else
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
--- models/article.js
+++ models/article.js
@@ -11,5 +11,9 @@ var article = new mongoose.Schema({
 article.statics.findByArticleId = function (id, callBack) {
   this.find({ _id: id }, callBack)
 }
+// 查询全部数据
+article.statics.findAll = function (callBack) {
+  this.find({}, callBack)
+}
 var articleModel = mongoose.model('article', article)
 module.exports = articleModel
